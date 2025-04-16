ac857ab0c10d22ddcf3d2a2b397798a77be19f2b
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2025-04-16 18:52:30 -0300

    Adding functionality: `CountDown` island

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,9 +7,11 @@ import * as $_app from "./routes/_app.tsx"
 import * as $about from "./routes/about.tsx"
 import * as $api_joke from "./routes/api/joke.ts"
 import * as $api_uuid from "./routes/api/uuid.ts"
+import * as $countdown from "./routes/countdown.tsx"
 import * as $greet_name_ from "./routes/greet/[name].tsx"
 import * as $index from "./routes/index.tsx"
 import * as $search from "./routes/search.tsx"
+import * as $CountDown from "./islands/CountDown.tsx"
 import * as $Counter from "./islands/Counter.tsx"
 import type { Manifest } from "$fresh/server.ts"
 
@@ -20,11 +22,13 @@ const manifest = {
     "./routes/about.tsx": $about,
     "./routes/api/joke.ts": $api_joke,
     "./routes/api/uuid.ts": $api_uuid,
+    "./routes/countdown.tsx": $countdown,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
     "./routes/search.tsx": $search,
   },
   islands: {
+    "./islands/CountDown.tsx": $CountDown,
     "./islands/Counter.tsx": $Counter,
   },
   baseUrl: import.meta.url,
